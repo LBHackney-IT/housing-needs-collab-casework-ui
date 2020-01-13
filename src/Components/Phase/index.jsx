5bc9a36f598a9123d053aec41e05d0680ff8787e
Ben Pirt <ben@pirt.co.uk>
2020-01-13 11:24:47 +0000

    Add correct feedback link

--- src/Components/Phase/index.jsx
+++ src/Components/Phase/index.jsx
@@ -9,8 +9,11 @@ export default class Phase extends Component {
             Alpha
           </strong>
           <span className="govuk-phase-banner__text">
-            This is a new service - your <a href="#/">feedback</a> will help us
-            to improve it.
+            This is a new service - your{' '}
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfPSZsFJVjg09Flmz4WF_2Tsg4jnIBvwQmSiahwR9LVMw4mEw/viewform">
+              feedback
+            </a>{' '}
+            will help us to improve it.
           </span>
         </p>
       </div>
